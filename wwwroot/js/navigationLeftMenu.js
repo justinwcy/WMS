e39e6db85a6166bf3ed45bed92ce3442656a1049
justinwcy <pythonemail20@gmail.com>
2024-05-22 16:23:20 +0800

    Fix login issues

---
+++ wwwroot/js/navigationLeftMenu.js
@@ -0,0 +1,32 @@
+﻿// to show the menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+
+});
+
+//to show the dropdown
+$(document).ready(function () {
+    $('.navDropdown').hover(function () {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+    }, function () {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+});
+
+// to animate the dropdown to the left side
+$(document).ready(function () {
+    $('.navDropdown').hover(function () {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').animate({
+            left: "200px"
+        }, 200);
+    }, function () {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').animate({
+            left: "0px"
+        }, 200);
+    });
+});
